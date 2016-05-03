5e2c07816fe21134b40c0fe454d50e15e345dff5
unknown <skl348@nyu.edu>
2016-05-03 18:16:51 -0400

    Fixed unclosed tags for <ul> and <div> in nav, added a separator between nav and page content, added css for news items. Cropped out bottom text in images to improve viewing experience

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/*
+$('.grid').masonry({
+  // options
+  itemSelector: '.grid-item',
+  columnWidth: '.grid-item'
+});
+*/
+
+/*
+var $container = $('.grid');
+
+$container.masonry({
+  // options
+  itemSelector: '.grid-item',
+  columnWidth: 400
+});
+*/
