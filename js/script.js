11afd26d66bf558c84d68bb529317ea515fa2689
thisissteve <skl348@nyu.edu>
2016-05-08 17:51:44 -0400

    Added js code to auto hide menu when area outside of nav is pressed. Added image assets for char profiles, added series template and Gundam 00 series page

--- js/script.js
+++ js/script.js
@@ -37,3 +37,12 @@ $("ul li a[href^='#']").on('click', function(e) {
      });
 
 });
+
+$(document).click(function (event) {
+    var clickover = $(event.target);
+    var $navbar = $(".navbar-collapse");               
+    var _opened = $navbar.hasClass("in");
+    if (_opened === true && !clickover.hasClass("navbar-toggle")) {      
+        $navbar.collapse('hide');
+    }
+});
