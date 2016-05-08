21b2286f699f0ced67d68f198d6ddaef744afc4f
thisissteve <skl348@nyu.edu>
2016-05-08 14:23:49 -0400

    Added a bunch of image assets for series. Added Gundam00 series page, has responsive breakpoints, added smooth scrolling functionality and scroll-spying for both side nav and mobile collapsed nav

--- js/script.js
+++ js/script.js
@@ -15,3 +15,25 @@ $container.masonry({
   columnWidth: 400
 });
 */
+
+// Smooth scrolling function - works on any ul li a link
+
+$("ul li a[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top
+     }, 500, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
